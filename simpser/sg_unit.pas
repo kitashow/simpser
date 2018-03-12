757abe3ead8cdf65963c375247d5dbdd6dd1a72f
Alexey <36297720+kitashow@users.noreply.github.com>
2018-03-13 00:13:13 +0300

    20180313.1

--- simpser/sg_unit.pas
+++ simpser/sg_unit.pas
@@ -63,17 +63,30 @@ type
 { TGamer1 }
 
 TGamer1 = class(avk_TFraim)
+private
+  FSprites: array of array of avk_TSkeletTile;
+private
+  function GetColAngle(const ACol: Integer): Single;
+  function GetColPosition(const ACol: Integer): zglTPoint2D;
+  function GetColVisible(const ACol: Integer): Integer;
+  function GetSprite(const ACol, ARow: Integer): avk_TSkeletTile;
+  procedure SetColAngle(const ACol: Integer; AValue: Single);
+  procedure SetColPosition(const ACol: Integer; AValue: zglTPoint2D);
+  procedure SetColVisible(const ACol: Integer; ARow: Integer);
+  procedure SetSprite(const ACol, ARow: Integer; AValue: avk_TSkeletTile = nil);
 public
-  FLegSprite: array [0..3] of avk_TSkeletTile;
-  FBodySprite: array [0..1] of avk_TSkeletTile;
   Position: zglTPoint2D;
   PosAngle: Single;
-  PosSkale: Single;
-  procedure SetNotHide(ALegNum, ABodyNum: Integer);
-  procedure UsePos;
+  PosScale: Single;
+  procedure SetPosForAll;
 public
-  procedure DoDraw(Sender: TObject);
-  procedure DoProc(Sender: TObject);
+  PosHide: boolean;
+  PosAnimate: boolean;
+public
+  property ColPosition[const ACol: Integer]: zglTPoint2D read GetColPosition write SetColPosition;
+  property ColAngle[const ACol: Integer]: Single read GetColAngle write SetColAngle;
+  property ColVisible[const ACol: Integer]: Integer read GetColVisible write SetColVisible;
+  property Sprite[const ACol, ARow: Integer]: avk_TSkeletTile read GetSprite write SetSprite;
 public
   constructor Create(const InParent: avk_TFraim = nil);
   destructor Destroy; override;
@@ -83,79 +96,113 @@ implementation
 
 { TGamer1 }
 
-procedure TGamer1.SetNotHide(ALegNum, ABodyNum: Integer);
-var
-  CKL: Integer;
+function TGamer1.GetColAngle(const ACol: Integer): Single;
 begin
-  //ноги
-  for CKL := 0 to SizeOf(FLegSprite) - 1 do
-    FLegSprite[CKL].Hide := not(CKL = ALegNum);
-  //тело
-  for CKL := 0 to SizeOf(FBodySprite) - 1 do
-    FBodySprite[CKL].Hide := not(CKL = ABodyNum);
+  Result := FSprites[ACol, 1].Angle; //первый и все
 end;
 
-procedure TGamer1.UsePos;
+procedure TGamer1.SetColAngle(const ACol: Integer; AValue: Single);
 var
   CKL: Integer;
 begin
-  //ноги
-  for CKL := 0 to SizeOf(FLegSprite) - 1 do
-    FLegSprite[CKL].SetInternalParameters(Position.X, Position.Y, PosAngle, PosSkale);
-  //тело
-  for CKL := 0 to SizeOf(FBodySprite) - 1 do
-    FBodySprite[CKL].SetInternalParameters(Position.X, Position.Y, PosAngle, PosSkale);
+  for CKL := 0 to Length(FSprites[ACol]) - 1 do
+    FSprites[ACol, CKL].Angle := AValue;
 end;
 
-procedure TGamer1.DoDraw(Sender: TObject);
-var
-  CKL: Integer;
+function TGamer1.GetColPosition(const ACol: Integer): zglTPoint2D;
 begin
-  //ноги
-  for CKL := 0 to SizeOf(FLegSprite) - 1 do
-    if not FLegSprite[CKL].Hide then
-       FLegSprite[CKL].DoDraw;
-  //тело
-  for CKL := 0 to SizeOf(FBodySprite) - 1 do
-    if not FBodySprite[CKL].Hide then
-      FBodySprite[CKL].DoDraw;
+  Result := FSprites[ACol, 1].Point; //первый и все
 end;
 
-procedure TGamer1.DoProc(Sender: TObject);
+procedure TGamer1.SetColPosition(const ACol: Integer; AValue: zglTPoint2D);
 var
   CKL: Integer;
 begin
-  for CKL := 0 to SizeOf(FLegSprite) - 1 do
-    if FLegSprite[CKL].Animate then
-       FLegSprite[CKL].DoProc;
+  for CKL := 0 to Length(FSprites[ACol]) - 1 do
+    FSprites[ACol, CKL].SetPoint(AValue.X, AValue.Y);
+end;
 
-  for CKL := 0 to SizeOf(FBodySprite) - 1 do
-    if FBodySprite[CKL].Animate then
-      FBodySprite[CKL].DoProc;
+function TGamer1.GetSprite(const ACol, ARow: Integer): avk_TSkeletTile;
+begin
+  if ACol > (Length(FSprites) - 1) then
+    SetSprite(ACol, ARow);
+
+  if ARow > (Length(FSprites[ACol]) - 1) then
+    SetSprite(ACol, ARow);
+
+  Result := FSprites[ACol, ARow];
+end;
+
+procedure TGamer1.SetSprite(const ACol, ARow: Integer; AValue: avk_TSkeletTile);
+var
+  PrevSize, CKL: Integer;
+begin
+  if ACol > (Length(FSprites) - 1) then
+    SetLength(FSprites, ACol + 1);
+
+  if ARow > (Length(FSprites[ACol]) - 1) then begin
+    PrevSize := Length(FSprites[ACol]) + 1;
+    for CKL := PrevSize to ARow - 1 do begin
+      SetLength(FSprites[ACol], CKL);
+      FSprites[ACol, CKL - 1] := avk_TSkeletTile.Create;
+    end;
+    SetLength(FSprites[ACol], ARow + 1);
+  end;
+
+  if Assigned(FSprites[ACol, ARow]) then
+    FSprites[ACol, ARow].Destroy;
+
+  FSprites[ACol, ARow] := AValue;
+
+  if not Assigned(FSprites[ACol, ARow]) then
+    FSprites[ACol, ARow] := avk_TSkeletTile.Create;
+end;
+
+function TGamer1.GetColVisible(const ACol: Integer): Integer;
+var
+  CKL: Integer;
+begin
+  Result := -1;
+  for CKL := 0 to Length(FSprites[ACol]) - 1 do
+    if not FSprites[ACol, CKL].Hide then Result := CKL;
+end;
+
+procedure TGamer1.SetColVisible(const ACol: Integer; ARow: Integer);
+var
+  CKL: Integer;
+begin
+  for CKL := 0 to Length(FSprites[ACol]) - 1 do
+    FSprites[ACol, CKL].Hide := not(CKL = ARow);
+end;
+
+procedure TGamer1.SetPosForAll;
+var
+  CKL1, CKL2: Integer;
+begin
+  for CKL1 := 0 to Length(FSprites) - 1 do
+    for CKL2 := 0 to Length(FSprites[CKL1]) - 1 do begin
+      FSprites[CKL1, CKL2].SetPoint(Position.X, Position.Y);
+      FSprites[CKL1, CKL2].Angle := PosAngle;
+      FSprites[CKL1, CKL2].Scale := PosScale;
+    end;
 end;
 
 constructor TGamer1.Create(const InParent: avk_TFraim);
-var
-  CKL: Integer;
 begin
   inherited Create(InParent);
-  for CKL := 0 to SizeOf(FLegSprite) - 1 do
-    FLegSprite[CKL] := avk_TSkeletTile.Create;
-  for CKL := 0 to SizeOf(FBodySprite) - 1 do
-    FBodySprite[CKL] := avk_TSkeletTile.Create;
-  OnDraw := DoDraw;
-  OnProc := DoProc;
+  SetLength(FSprites, 0);
+  //OnDraw := DoDraw;
+  //OnProc := DoProc;
 end;
 
 destructor TGamer1.Destroy;
 var
-  CKL: Integer;
+  CKL0, CKL1: Integer;
 begin
-  for CKL := 0 to SizeOf(FLegSprite) - 1 do
-    FLegSprite[CKL].Destroy;
-
-  for CKL := 0 to SizeOf(FBodySprite) - 1 do
-    FBodySprite[CKL].Destroy;
+  for CKL0 := 0 to Length(FSprites) - 1 do
+    for CKL1 := 0 to Length(FSprites[CKL0]) - 1 do begin
+      FSprites[CKL0, CKL1].Destroy;
+    end;
 
   inherited Destroy;
 end;
